7de920651c20b68c75d9adeb3520110ed171823f
Dmitry Kurganov <dimacup30@gmail.com>
2023-08-19 22:24:19 +0300

    almost done

--- src/main/java/FilmManager.java
+++ src/main/java/FilmManager.java
@@ -1,9 +1,9 @@
 public class FilmManager {
 
-    String[] films = {"1", "2", "3", "4"
+    String[] films = {"Гадкий я", "Тачки", "Интерстеллар", "Марсианин"
     };
 
-    public void newFilm(String name) {
+    public String[] newFilm(String name) {
 
         String[] tmp = new String[films.length + 1];
         for (int i = 0; i < films.length; i++) {
@@ -11,9 +11,18 @@ public class FilmManager {
         }
         tmp[tmp.length - 1] = name;
         films = tmp;
+        return films;
     }
 
     public String[] returnFilms() {
         return films;
+    }
+
+    public String[] reverseReturnFilms() {
+        String[] reverse = new String[films.length];
+        for (int i = 0; i < reverse.length; i++) {
+            reverse[i] = films[films.length - 1 - i];
+        }
+        return reverse;
     }
 }
--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -4,11 +4,31 @@ import org.junit.jupiter.api.Test;
 public class FilmManagerTest {
 
     @Test
-    public void Test() {
+    public void Test1() {
         FilmManager filmManager = new FilmManager();
-        filmManager.newFilm("BoB");
+        filmManager.newFilm("Чужой");
 
-        filmManager.returnFilms();
     }
 
+    @Test
+    public void Test2() {
+        FilmManager filmManager = new FilmManager();
+        filmManager.newFilm("Чужой");
+        filmManager.returnFilms();
+        String[] expected = {"Гадкий я", "Тачки", "Интерстеллар", "Марсианин", "Чужой"};
+        String[] actual = filmManager.films;
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void Test3() {
+        FilmManager filmManager = new FilmManager();
+        filmManager.newFilm("Свой");
+        String[] expected = {"Свой", "Марсианин", "Интерстеллар", "Тачки", "Гадкий я"};
+        String[] actual = filmManager.reverseReturnFilms();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
